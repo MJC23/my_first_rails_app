372d7fe5cb0629ea26ce83213dd4b05d9518a25e
MJC23 <mcontreras@gmail.com>
2019-08-22 14:36:22 -0700

    updates for heroku deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,7 +110,3 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 end
-#config/environments/production.rb
-...
-config.require_master_key = true
-...
